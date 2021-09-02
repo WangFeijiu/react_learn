c4cc4f64b368f840572e83c6c11bf08bc8781950
wangfeijiu <1762509314@qq.com>
2021-09-02 17:28:33 +0800

    feat: 初始化

---
+++ src/pages/components/useRequest/index.tsx
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-08-26 11:11:01
+ * @LastEditTime: 2021-08-30 11:41:13
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \react_learn\src\pages\components\useRequest\index.tsx
+ */
+import Mock from 'mockjs';
