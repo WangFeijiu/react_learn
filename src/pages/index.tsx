d0369d361f5d1a07a3c413433ac30b5decb0102b
wangfeijiu <1762509314@qq.com>
2021-11-15 10:20:44 +0800

    feat: vconsole

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2021-08-02 14:45:10
- * @LastEditTime: 2021-08-10 09:22:46
+ * @LastEditTime: 2021-09-02 17:44:56
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \react_learn\src\pages\index.tsx
  */
+import VConsole from 'vconsole';
+
+const vConsole = new VConsole();
+
 export default function IndexPage() {
   return <div>1</div>;
 }
---
+++ src/services/req.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-02 17:44:29
+ * @LastEditTime: 2021-09-17 21:11:46
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \react_learn\src\services\req.ts
+ */
